c53dacdfbd5e7074041f17119d8b3359a8eeb36d
liweigu <liweigu714@163com>
2019-01-04 15:23:29 +0800

    实现保存和加载模型

--- model/src/main/java/research/model/recommend/RecommendModel.java
+++ model/src/main/java/research/model/recommend/RecommendModel.java
@@ -1,7 +1,14 @@
 package research.model.recommend;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.util.ModelSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 推荐模型
@@ -10,6 +17,7 @@ import java.util.Map;
  *
  */
 public abstract class RecommendModel implements RecommendModelI {
+	static Logger Log = LoggerFactory.getLogger(RecommendModel.class);
 	static Object Model;
 	static Map<String, Object> InitProps = new HashMap<String, Object>();
 
@@ -48,8 +56,24 @@ public abstract class RecommendModel implements RecommendModelI {
 	 * 
 	 * @param path 模型路径
 	 */
-	public void save(String path) {
-		throw new UnsupportedOperationException();
+	public boolean save(String path) {
+		boolean flag = false;
+
+		if (Model != null) {
+			if (Model instanceof ComputationGraph) {
+				ComputationGraph computationGraph = (ComputationGraph) Model;
+				File modelFile = new File(path);
+				try {
+					// writeModel会自动删除旧的模型文件
+					ModelSerializer.writeModel(computationGraph, modelFile, true);
+					flag = true;
+				} catch (IOException e) {
+					Log.info("保存模型失败", e);
+				}
+			}
+		}
+
+		return flag;
 	}
 
 	/**
@@ -57,7 +81,35 @@ public abstract class RecommendModel implements RecommendModelI {
 	 * 
 	 * @param path 模型路径
 	 */
-	public void restore(String path) {
-		throw new UnsupportedOperationException();
+	public boolean restore(String path) {
+		boolean flag = false;
+
+		if (Model == null) {
+			if (path != null && path.length() > 0) {
+				if (path.startsWith("http://")) {
+					// TODO: 下载预训练模型
+					throw new UnsupportedOperationException();
+				} else {
+					File modelFile = new File(path);
+					if (modelFile.exists()) {
+						// TODO: 识别并加载不同类型的模型
+						try {
+							Model = ModelSerializer.restoreComputationGraph(modelFile, true);
+							flag = true;
+						} catch (IOException e) {
+							Log.info("加载模型失败", e);
+						}
+					} else {
+						Log.info("加载模型失败，模型文件不存在：path = " + path);
+					}
+				}
+			} else {
+				Log.info("加载模型失败：path为空");
+			}
+		} else {
+			Log.info("加载模型失败：模型不为空，初始化过的模型不能再加载。");
+		}
+
+		return flag;
 	}
 }
